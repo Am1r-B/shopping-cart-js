39f290331e7a108246a0e1d7f5645d3539576b41
Am1r-B <69463011+Am1r-B@users.noreply.github.com>
2023-05-13 23:20:09 +0330

    Make add cards dynamically to page

---
+++ main.js
@@ -0,0 +1,58 @@
+const shop = document.getElementById("shop");
+
+const shopItemsData = [
+  {
+    id: 1,
+    name: "Casual Shirt",
+    price: 45,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-1.jpg",
+  },
+  {
+    id: 2,
+    name: "Office Shirt",
+    price: 100,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-2.jpg",
+  },
+  {
+    id: 3,
+    name: "T Shirt",
+    price: 25,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-3.jpg",
+  },
+  {
+    id: 4,
+    name: "Mens Suit",
+    price: 300,
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+    img: "images/img-4.jpg",
+  },
+];
+
+const generateShop = () => {
+  return (shop.innerHTML = shopItemsData
+    .map((item) => {
+      const { id, name, price, desc, img } = item;
+      return `
+      <div id="product-${id}" class="item">
+        <img width="220" height="220" src=${img} alt="" />
+        <div class="details">
+          <h3>${name}</h3>
+          <p>${desc}</p>
+          <div class="price-quantity">
+            <h2>$ ${price}</h2>
+            <div class="buttons">
+              <i class="bi bi-dash-lg"></i>
+              <div id="quantity-${id}" class="quantity">0</div>
+              <i class="bi bi-plus-lg"></i>
+            </div>
+          </div>
+        </div>
+      </div>`;
+    })
+    .join(""));
+};
+
+generateShop();
