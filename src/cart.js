8945ac62f64a1f845e5dae8a26fe13fab94c88ff
Am1r-B <69463011+Am1r-B@users.noreply.github.com>
2023-05-14 23:56:53 +0330

    Add markups for empty cart with styles

--- src/cart.js
+++ src/cart.js
@@ -1,10 +1,28 @@
+const label = document.getElementById("label");
+const shoppingCart = document.getElementById("shopping-cart");
+
 let basket = JSON.parse(localStorage.getItem("shopping-cart")) || [];
 
 const calculation = () => {
   const cartIcon = document.getElementById("cart-amount");
-  cartIcon.innerHTML = basket.reduce((sum, item) => {
-    return sum + item.amount;
+  cartIcon.innerHTML = basket.reduce((sum, cartItem) => {
+    return sum + cartItem.amount;
   }, 0);
 };
 
 calculation();
+
+const generateCartItems = () => {
+  if (basket.length !== 0) {
+  } else {
+    label.innerHTML = `
+      <h2>Cart is Empty</h2>
+      <a href="index.html">
+        <button class="home-btn">Back to home</button>
+      </a>
+    `;
+    shoppingCart.innerHTML = ``;
+  }
+};
+
+generateCartItems();
