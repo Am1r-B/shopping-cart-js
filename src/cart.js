91a76d01a01ee89563dec7e13f19d91d51641ab0
Am1r-B <69463011+Am1r-B@users.noreply.github.com>
2023-05-15 06:21:06 +0330

    Add clear and checkout buttons under total with style & clean code

--- src/cart.js
+++ src/cart.js
@@ -14,42 +14,36 @@ calculation();
 
 const generateCartItems = () => {
   if (basket.length !== 0) {
-    return (shoppingCart.innerHTML = basket
+    shoppingCart.innerHTML = basket
       .map(({ id, amount }) => {
         const { name, price, img } =
           shopItemsData.find((item) => item.id === id) || [];
         return `
-        <div class="cart-item">
-          <img width="110" height="110" src="${img}" alt="" />
-          <div class="details">
+          <div class="cart-item">
+            <img width="110" height="110" src="${img}" alt="" />
+            <div class="details">
 
-            <div class="title-price-x">
-              <h4 class="title-price">
-                <span>${name}</span>
-                <span class="cart-item-price">$ ${price}</span>
-              </h4>
-              <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
+              <div class="title-price-x">
+                <h4 class="title-price">
+                  <span>${name}</span>
+                  <span class="cart-item-price">$ ${price}</span>
+                </h4>
+                <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
+              </div>
+
+              <div class="buttons">
+                <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                <div id="quantity-${id}" class="quantity">${amount}</div>
+                <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+              </div>
+
+              <h3>$ ${amount * price}</h3>
             </div>
-
-            <div class="buttons">
-              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
-              <div id="quantity-${id}" class="quantity">${amount}</div>
-              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
-            </div>
-
-            <h3>$ ${amount * price}</h3>
           </div>
-        </div>
-      `;
+        `;
       })
-      .join(""));
+      .join("");
   } else {
-    label.innerHTML = `
-      <h2>Cart is Empty</h2>
-      <a href="index.html">
-        <button class="home-btn">Back to home</button>
-      </a>
-    `;
     shoppingCart.innerHTML = ``;
   }
 };
@@ -91,6 +85,15 @@ const totalAmount = () => {
     }, 0);
     label.innerHTML = `
       <h2>Total Bill: $ ${total}</h2>
+      <button class="checkout">Checkout</button>
+      <button onclick="clearCart()" class="remove-all">Clear Cart</button>
+    `;
+  } else {
+    label.innerHTML = `
+      <h2>Cart is Empty</h2>
+      <a href="index.html">
+        <button class="home-btn">Back to home</button>
+      </a>
     `;
   }
 };
